c393b1a761ddaee5867b825d3f8c01d85013be64
Valentin Saugnier <valentin.s.10@gmail.com>
2019-08-02 15:33:49 +0200

    :wrench: Add option to hide cancel button

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -4,23 +4,23 @@ import { saveAs } from 'file-saver';
 @Component({
     selector: 'app-root',
     template: `
-        <h1>ngx-image-drawing demo app !</h1>
+        <h1><a target="_blank" href="https://github.com/GroupeCurious/ngx-image-drawing">ngx-image-drawing</a> demo app
+            !</h1>
 
         <image-drawing width="800" height="600"
                        borderCss="1px solid black"
+                       [showCancelButton]="false"
+                       saveBtnText="Save the image as JPEG !"
                        enableLoadAnotherImage="true" enableRemoveImage="true"
                        src="https://images.unsplash.com/photo-1537984827217-96d3397ba7ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80"
-                       (cancel)="cancelBtn()" (save)="saveBtn($event)"
-        ></image-drawing>`,
+                       (save)="saveBtn($event)"
+        >
+        </image-drawing>`,
     styles: []
 })
 export class AppComponent {
 
     public saveBtn($event) {
         saveAs($event, 'image.jpg');
-    }
-
-    public cancelBtn() {
-        alert('Nothing to do');
     }
 }
